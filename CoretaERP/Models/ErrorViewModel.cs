d4cb390bb5f8eb7a667f1f8a6c47860c6a97d314
Anderson-BM <andersonbatistamateo@gmail.com>
2026-02-01 23:01:31 -0400

    hola

---
+++ CoretaERP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoretaERP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
